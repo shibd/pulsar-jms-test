8c05dc8795674cf431d886e34bc348c18096961d
Baodi Shi <baodi@apache.org>
2026-04-09 11:40:22 +0800

    Use sn lib

--- src/main/java/io/streamnative/jms/JmsProducer2.java
+++ src/main/java/io/streamnative/jms/JmsProducer2.java
@@ -1,6 +1,6 @@
 package io.streamnative.jms;
 
-import com.datastax.oss.pulsar.jms.PulsarConnectionFactory;
+import io.streamnative.pulsar.jms.PulsarConnectionFactory;
 import java.util.HashMap;
 import java.util.Map;
 import javax.jms.JMSContext;
@@ -20,11 +20,11 @@ import javax.jms.Topic;
 public class JmsProducer2 {
 
     private static final String BROKER_SERVICE_URL =
-            "";
+            "pulsar+ssl://pc-c3720b92.aws-use2-production-snci-pool-kid.streamnative.aws.snio.cloud:6651";
     private static final String WEB_SERVICE_URL =
-            "";
+            "https://pc-c3720b92.aws-use2-production-snci-pool-kid.streamnative.aws.snio.cloud";
     private static final String TOKEN =
-            "";
+            "eyJhbGciOiJSUzI1NiIsImtpZCI6InByb2R1Y3Rpb24ta2V5LTIwMjUtMTItdjEiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOlsidXJuOnNuOmNsb3VkOnNuY2xvdWQiXSwiZXhwIjoxNzc4MjA4OTM5LCJpYXQiOjE3NzU2MTY5NDAsImlzcyI6Imh0dHBzOi8vYXBpa2V5cy5zdHJlYW1uYXRpdmUuY2xvdWQvIiwianRpIjoiMGYxMzQ4YzU5ZGE0NDRkZWExOWM2ODM2OTk4MDM5ZTIiLCJzY29wZSI6W10sInN1YiI6ImFkbWluQHNuY2xvdWQuYXV0aC5zdHJlYW1uYXRpdmUuY2xvdWQifQ.HdnAGo13NF3458uYTMS7Mur807ZYdDoGRCZx6q_gtCnp64ybXGoE12qrsRG3t4l3lQDLoPfrJMD9oGVahs_XpOxRG_wKv2kgQxTkyJuYf3SMk1bpMoP-UJNWIk-FBvPAQT_rc59-uRxOXO9JWM2QyaqR1ze0zUtwOqppvk979rjdcDrchbOaRxh_4F6-9Iy9j4LIwIzsPaJnoWnPNamI3UblfjQpVy-nQYh3PEILicLLoKeegOgSbVpFh6jOUBs2CgDiW0U6wQBgPuW7HlVHtoOZyYoL1XiSV4WtKQ6L6oyLcUtnlIG0pLzAL9u6NO6AaekdiG5Uo5w06QPi-wT7zcbt77vls4nVI1ZGFpIv-zyAUC9XCbPIRKn3TD8o7N_VpbpNG7DVwHKgnT0s1iPEf4zlaDXGF8d9Xf6_K4xFIAk4vKYHqXzhcsc6EyzDLPtUIUQArelqEAO0MIKBSmlJEKus8KEr4umFX5JU7YGSxS0rWCubDO4YdIirZDzuPkuULUbVFa7avFcfHqOmQ3BIYnETeTyUz6WXKDJn88PtXRt4NWCCiIdt82qSbcs8-SlmMVaZYsJ7VtU8soNpCIetGT5FKxDGxNua1LAholmUafTIJjp0XneYiUhGEfc_zA9WPucGMkclg9WtUnPKNW1rKf7XrnAF-iSQTXO3Bsl5cOo";
 
     public static void main(String[] args) throws Exception {
         String topicName   = args.length > 0 ? args[0] : "persistent://public/default/input-2";
